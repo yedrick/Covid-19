ce0120adf34384b2ed353dc9cacf53fcfbc2a537
yerson <39100650+yedrick@users.noreply.github.com>
2020-07-26 23:09:49 -0400

    Add files via upload

---
+++ javaScript/script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    var x=document.getElementById("covid");
+    var y=document.getElementById('body');
+    x.style.display="none"
+    x.style.display="hidden"
+    y.classList.remove("hidden");
+  }
+  
+ 
